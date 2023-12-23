fef5fc95b967dbc19a51ef258d53bb6a43ab4d01
Alex Marcus <mr@alexmarc.us>
2023-12-23 11:10:05 -0800

    Test fixes

--- test/FTData.test.js
+++ test/FTData.test.js
@@ -27,20 +27,20 @@ describe('FTData.getProgress', () => {
   });
 
   it('should return 0.5 when halfway between start and ETA times', () => {
-    const epochEta = Date.now() + 1000;
-    const epochLeftDock = Date.now() - 1000;
+    const epochEta = (Date.now() / 1000) + 1000;
+    const epochLeftDock = (Date.now() / 1000) - 1000;
     expect(FTData.getProgress(false, epochEta, epochLeftDock)).to.equal(0.5);
   });
 
   it('should return ~0.9 when nearly arrived', () => {
-    const epochEta = Date.now() + 100;
-    const epochLeftDock = Date.now() - 1000;
+    const epochEta = (Date.now() / 1000) + 100;
+    const epochLeftDock = (Date.now() / 1000) - 1000;
     expect(FTData.getProgress(false, epochEta, epochLeftDock)).to.be.greaterThanOrEqual(0.9);
   });
 
   it('should return ~0.1 when just left', () => {
-    const epochEta = Date.now() + 1000;
-    const epochLeftDock = Date.now() - 100;
+    const epochEta = (Date.now() / 1000) + 1000;
+    const epochLeftDock = (Date.now() / 1000) - 100;
     expect(FTData.getProgress(false, epochEta, epochLeftDock)).to.be.lessThanOrEqual(0.1 );
   });
 });

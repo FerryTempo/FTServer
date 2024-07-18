2cfe6e3e64a95dc7d1e62e50637436b8245ab462
Craig Kulfan <craigkulfan@gmail.com>
2024-07-18 09:08:35 -0700

    removed fetch boat details from wsdot

--- src/WSDOT.js
+++ src/WSDOT.js
@@ -18,14 +18,3 @@ export const fetchVesselData = async function() {
         return response.json();
       });
 };
-
-export const fetchVesselDetail = async function(vesselId) {
-  return fetch(`https://www.wsdot.wa.gov/ferries/api/vessels/rest/vesselbasics/${vesselId}?apiaccesscode=${process.env.WSDOT_API_KEY}`)
-  .then((response) => {
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-
-    return response.json();
-  });
-};

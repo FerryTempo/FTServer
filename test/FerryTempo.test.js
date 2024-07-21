874d33983985df799d496a19674ef71657d11898
Craig Kulfan <craigkulfan@hotmail.com>
2024-07-21 09:51:38 -0700

    Added basic unit test to FerryTempo.js

--- test/FerryTempo.test.js
+++ test/FerryTempo.test.js
@@ -1,117 +1,210 @@
+
+import routeFTData from '../data/RouteFTData.js';
 import {
-    getCurrentEpochSeconds,
-    getEpochSecondsFromWSDOT,
-    getHumanDateFromEpochSeconds,
-    getProgress,
-    updateAverage,
-    getAverage,
-    getRouteFromTerminals
-  } from './Utils.js';
-  import routeFTData from '../data/RouteFTData.js';
-  import routePositionData from '../data/RoutePositionData.js';
-  import Logger from './Logger.js';
-  import processFerryData from './path_to_your_code.js';
-  
-  jest.mock('./Utils.js');
-  jest.mock('../data/RouteFTData.js');
-  jest.mock('../data/RoutePositionData.js');
-  jest.mock('./Logger.js');
-  
-  describe('processFerryData', () => {
-    let logger;
-    let vesselData;
-  
-    beforeEach(() => {
-      logger = new Logger();
-      jest.clearAllMocks();
-      vesselData = [
-        {
-          VesselID: 1,
-          VesselName: 'Vessel 1',
-          DepartingTerminalID: 1,
-          DepartingTerminalName: 'Terminal A',
-          DepartingTerminalAbbrev: 'TA',
-          ArrivingTerminalName: 'Terminal B',
-          ArrivingTerminalAbbrev: 'TB',
-          Latitude: 47.6062,
-          Longitude: -122.3321,
-          Speed: 15,
-          Heading: 90,
-          InService: true,
-          AtDock: false,
-          LeftDock: '2021-12-01T12:00:00Z',
-          Eta: '2021-12-01T12:30:00Z',
-          ScheduledDeparture: '2021-12-01T12:00:00Z',
-          OpRouteAbbrev: ['AB'],
-          VesselPositionNum: 1,
-          TimeStamp: '2021-12-01T12:05:00Z',
-          VesselWatchShutID: null,
-          VesselWatchShutMsg: '',
-          VesselWatchShutFlag: 0,
+  getCurrentEpochSeconds,
+  getEpochSecondsFromWSDOT,
+  getHumanDateFromEpochSeconds,
+  getProgress,
+  updateAverage,
+  getAverage,
+  getRouteFromTerminals
+} from '../src/Utils.js';
+import routePositionData from '../data/RoutePositionData.js';
+import Logger from '../src/Logger.js';
+import FerryTempo from '../src/FerryTempo.js';
+import { jest } from '@jest/globals'
+
+// jest.mock('../src/Utils.js');
+// jest.mock('../data/RoutePositionData.js');
+// jest.mock('../src/Logger.js');
+jest.mock('../data/RouteFTData.js', () => ({
+  'sea-bi': {
+    'boatData': {},
+    'routeID': '5',
+    'portData': {
+      'portES': {
+        'TerminalName': 'Seattle',
+        'TerminalAbbrev': 'P52',
+        'TerminalID': 7,
+        'BoatAtDock': false,
+        'NextScheduledSailing': null,
+        'PortDepartureDelay': null,
+        'PortETA': null,
+      },
+      'portWN': {
+        'TerminalName': 'Bainbridge Island',
+        'TerminalAbbrev': 'BBI',
+        'TerminalID': 3,
+        'BoatAtDock': false,
+        'NextScheduledSailing': null,
+        'PortDepartureDelay': null,
+        'PortETA': null,
+      },
+    },
+  }
+}));
+
+describe('processFerryData', () => {
+  let logger;
+  let vesselData;
+  let routeData;
+
+  beforeEach(() => {
+    logger = new Logger();
+    jest.clearAllMocks();
+    vesselData = [
+      {
+        "VesselID": 74,
+        "VesselName": "Chimacum",
+        "Mmsi": 367712660,
+        "DepartingTerminalID": 7,
+        "DepartingTerminalName": "Seattle",
+        "DepartingTerminalAbbrev": "P52",
+        "ArrivingTerminalID": 3,
+        "ArrivingTerminalName": "Bainbridge Island",
+        "ArrivingTerminalAbbrev": "BBI",
+        "Latitude": 47.602505,
+        "Longitude": -122.34021,
+        "Speed": 0,
+        "Heading": 85,
+        "InService": true,
+        "AtDock": true,
+        "LeftDock": null,
+        "Eta": null,
+        "EtaBasis": null,
+        "ScheduledDeparture": "/Date(1721417100000-0700)/",
+        "OpRouteAbbrev": [
+          "sea-bi"
+        ],
+        "VesselPositionNum": 1,
+        "SortSeq": 60,
+        "ManagedBy": 1,
+        "TimeStamp": "/Date(1721417003000-0700)/",
+        "VesselWatchShutID": 11,
+        "VesselWatchShutMsg": "Vessel information unavailable. ",
+        "VesselWatchShutFlag": "0",
+        "VesselWatchStatus": "0",
+        "VesselWatchMsg": "WSF's VesselWatch page is currently not responding and is out of service. Thank you for your patience while we work to restore this page. "
+      },
+      {
+        "VesselID": 32,
+        "VesselName": "Tacoma",
+        "Mmsi": 366772760,
+        "DepartingTerminalID": 3,
+        "DepartingTerminalName": "Bainbridge Island",
+        "DepartingTerminalAbbrev": "BBI",
+        "ArrivingTerminalID": 7,
+        "ArrivingTerminalName": "Seattle",
+        "ArrivingTerminalAbbrev": "P52",
+        "Latitude": 47.62246,
+        "Longitude": -122.509092,
+        "Speed": 0,
+        "Heading": 282,
+        "InService": true,
+        "AtDock": true,
+        "LeftDock": null,
+        "Eta": null,
+        "EtaBasis": null,
+        "ScheduledDeparture": "/Date(1721416800000-0700)/",
+        "OpRouteAbbrev": [
+          "sea-bi"
+        ],
+        "VesselPositionNum": 2,
+        "SortSeq": 60,
+        "ManagedBy": 1,
+        "TimeStamp": "/Date(1721417004000-0700)/",
+        "VesselWatchShutID": 20,
+        "VesselWatchShutMsg": "Vessel information unavailable",
+        "VesselWatchShutFlag": "0",
+        "VesselWatchStatus": "0",
+        "VesselWatchMsg": "WSF's VesselWatch page is currently not responding and is out of service. Thank you for your patience while we work to restore this page. "
+      }
+    ];
+    routeData = {
+      "sea-bi": {
+        "boatData": {
+          "boat1": {
+            "ArrivalTimeMinus": 0,
+            "ArrivingTerminalAbbrev": "BBI",
+            "ArrivingTerminalName": "Bainbridge Island",
+            "AtDock": true,
+            "BoatDepartureDelay": 0,
+            "BoatETA": 0,
+            "DepartingTerminalName": "Seattle",
+            "DepartingTerminalAbbrev": "P52",
+            "DepartureDelayAverage": 0,
+            "Direction": "WN",
+            "Heading": 85,
+            "InService": true,
+            "LeftDock": 0,
+            "OnDuty": true,
+            "PositionUpdated": 1721417003,
+            "Progress": 0,
+            "ScheduledDeparture": 1721417100,
+            "Speed": 0,
+            "StopTimer": 67,
+            "VesselName": "Chimacum",
+            "VesselPosition": 1
+          },
+          "boat2": {
+            "ArrivalTimeMinus": 0,
+            "ArrivingTerminalAbbrev": "P52",
+            "ArrivingTerminalName": "Seattle",
+            "AtDock": true,
+            "BoatDepartureDelay": 204,
+            "BoatETA": 0,
+            "DepartingTerminalName": "Bainbridge Island",
+            "DepartingTerminalAbbrev": "BBI",
+            "DepartureDelayAverage": 0,
+            "Direction": "ES",
+            "Heading": 282,
+            "InService": true,
+            "LeftDock": 0,
+            "OnDuty": true,
+            "PositionUpdated": 1721417004,
+            "Progress": 0,
+            "ScheduledDeparture": 1721416800,
+            "Speed": 0,
+            "StopTimer": 75,
+            "VesselName": "Tacoma",
+            "VesselPosition": 2
+          }
+        },
+        "routeID": "5",
+        "portData": {
+          "portES": {
+            "TerminalName": "Seattle",
+            "TerminalAbbrev": "P52",
+            "TerminalID": 7,
+            "BoatAtDock": true,
+            "NextScheduledSailing": null,
+            "PortDepartureDelay": 0,
+            "PortETA": 0,
+            "PortStopTimer": 67,
+            "PortDepartureDelayAverage": 0,
+            "PortArrivalTimeMinus": 0
+          },
+          "portWN": {
+            "TerminalName": "Bainbridge Island",
+            "TerminalAbbrev": "BBI",
+            "TerminalID": 3,
+            "BoatAtDock": true,
+            "NextScheduledSailing": null,
+            "PortDepartureDelay": 0,
+            "PortETA": 0,
+            "PortArrivalTimeMinus": 0,
+            "PortStopTimer": 75,
+            "PortDepartureDelayAverage": 0
+          }
         }
-      ];
-    });
-  
-    it('should process vessel data correctly', () => {
-      getEpochSecondsFromWSDOT.mockReturnValueOnce(1638360000) // LeftDock
-        .mockReturnValueOnce(1638361800) // Eta
-        .mockReturnValueOnce(1638360300) // TimeStamp
-        .mockReturnValueOnce(1638360000) // ScheduledDeparture
-        .mockReturnValueOnce(1638360300); // TimeStamp
-  
-      getCurrentEpochSeconds.mockReturnValue(1638360300);
-      getProgress.mockReturnValue(0.5);
-      getAverage.mockReturnValue(10);
-      updateAverage.mockReturnValue(15);
-      getRouteFromTerminals.mockReturnValue('AB');
-  
-      const result = processFerryData(vesselData);
-  
-      expect(result).toEqual(expect.any(Object));
-      expect(getEpochSecondsFromWSDOT).toHaveBeenCalledTimes(5);
-      expect(getCurrentEpochSeconds).toHaveBeenCalledTimes(1);
-      expect(getProgress).toHaveBeenCalledTimes(1);
-      expect(getAverage).toHaveBeenCalledTimes(2);
-      expect(updateAverage).toHaveBeenCalledTimes(1);
-      expect(getRouteFromTerminals).toHaveBeenCalledTimes(1);
-    });
-  
-    it('should handle vessels not in service', () => {
-      vesselData[0].InService = false;
-  
-      const result = processFerryData(vesselData);
-  
-      expect(result).toEqual(expect.any(Object));
-      expect(result['AB'].boatData.boat1.InService).toBe(false);
-    });
-  
-    it('should compute route if OpRouteAbbrev is null', () => {
-      vesselData[0].OpRouteAbbrev = null;
-  
-      getRouteFromTerminals.mockReturnValue('AB');
-      const result = processFerryData(vesselData);
-  
-      expect(getRouteFromTerminals).toHaveBeenCalledWith('Terminal A', 'Terminal B');
-      expect(result).toEqual(expect.any(Object));
-    });
-  
-    it('should log debug messages when VesselWatchShutFlag is non-zero', () => {
-      vesselData[0].VesselWatchShutFlag = 1;
-      vesselData[0].VesselWatchShutMsg = 'Test Message';
-  
-      processFerryData(vesselData);
-  
-      expect(logger.debug).toHaveBeenCalledWith('Vessel 1 has message: Test Message');
-    });
-  
-    it('should handle vessels at dock', () => {
-      vesselData[0].AtDock = true;
-  
-      getCurrentEpochSeconds.mockReturnValue(1638360300);
-      const result = processFerryData(vesselData);
-  
-      expect(result).toEqual(expect.any(Object));
-      expect(result['AB'].boatData.boat1.AtDock).toBe(true);
-    });
+      }
+    }
   });
-  
+
+  it('should process vessel data correctly', () => {
+    const result = FerryTempo.processFerryData(vesselData);
+
+    // static test taken from recent server data
+    expect(result['sea-bi']).toEqual(routeData['sea-bi']);
+  });
+});

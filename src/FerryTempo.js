bbf3118ed083085cdc619b9108b7839430281083
Craig Kulfan <craigkulfan@hotmail.com>
2024-09-28 15:54:44 -0700

    fixed case sensitive search

--- src/FerryTempo.js
+++ src/FerryTempo.js
@@ -108,8 +108,9 @@ export default {
         }
       }
 
-      // Calculating if a boat is on duty by looking at the system shut flag and message. If the flag is not 0 and the message is out of service, then the boat is not onDuty
-      const onDuty = InService  ? !(VesselWatchShutFlag != 0 && VesselWatchShutMsg.includes('Vessel Out of Service')) : false;
+      // Calculating if a boat is on duty by looking at the system shut flag and message. If the flag is not 0 and the message is out of service, 
+      // then the boat is not onDuty
+      const onDuty = InService  ? !(VesselWatchShutFlag != 0 && VesselWatchShutMsg.toLowerCase().includes('vessel out of service')) : false;
 
       // Debug message for when a boat is going to/from the Fuel Dock, which should be considered out of service. (set to info to catch on server)
       if (onDuty && (ArrivingTerminalAbbrev === 'P15' || DepartingTerminalAbbrev === 'P15')) {
